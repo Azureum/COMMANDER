1c31534b5e43d52a69076a17bb1083e53ae64b21
Azurieum <mlgopnope@gmail.com>
2024-06-27 15:27:20 -0400

    made buttons

--- app/access/page.tsx
+++ app/access/page.tsx
@@ -4,9 +4,41 @@ import React from "react";
 
 export default function Home() {
     return (
-        <main>
-            <p>hello!</p>
+        <main className="flex items-center justify-center h-screen">
 
+            
+
+        <div className="flex flex-col items-center">
+    <div className="grid grid-cols-2 gap-2">
+        <button className="shadow-[0_0_0_3px_#000000_inset] px-6 py-2 bg-transparent border border-black dark:border-white dark:text-white text-black rounded-lg font-bold transform hover:-translate-y-1 transition duration-400">
+            Restart PC
+        </button>
+        <button className="shadow-[0_0_0_3px_#000000_inset] px-6 py-2 bg-transparent border border-black dark:border-white dark:text-white text-black rounded-lg font-bold transform hover:-translate-y-1 transition duration-400">
+            Restart Wifi
+        </button>
+        <button className="shadow-[0_0_0_3px_#000000_inset] px-6 py-2 bg-transparent border border-black dark:border-white dark:text-white text-black rounded-lg font-bold transform hover:-translate-y-1 transition duration-400">
+            Shutdown PC
+        </button>
+        <button className="shadow-[0_0_0_3px_#000000_inset] px-6 py-2 bg-transparent border border-black dark:border-white dark:text-white text-black rounded-lg font-bold transform hover:-translate-y-1 transition duration-400">
+            Turn Off Screen
+        </button>
+        <button className="shadow-[0_0_0_3px_#000000_inset] px-6 py-2 bg-transparent border border-black dark:border-white dark:text-white text-black rounded-lg font-bold transform hover:-translate-y-1 transition duration-400">
+            Macro 1
+        </button>
+        <button className="shadow-[0_0_0_3px_#000000_inset] px-6 py-2 bg-transparent border border-black dark:border-white dark:text-white text-black rounded-lg font-bold transform hover:-translate-y-1 transition duration-400">
+            Macro 2
+        </button>
+        <button className="shadow-[0_0_0_3px_#000000_inset] px-6 py-2 bg-transparent border border-black dark:border-white dark:text-white text-black rounded-lg font-bold transform hover:-translate-y-1 transition duration-400">
+            Macro 3
+        </button>
+        <button className="shadow-[0_0_0_3px_#000000_inset] px-6 py-2 bg-transparent border border-black dark:border-white dark:text-white text-black rounded-lg font-bold transform hover:-translate-y-1 transition duration-400">
+            Macro 4
+        </button>
+    </div>
+    <button className="mt-4 shadow-[0_0_0_3px_#000000_inset] px-6 py-2 bg-transparent border border-black dark:border-white dark:text-white text-black rounded-lg font-bold transform hover:-translate-y-1 transition duration-400">
+        New Button
+    </button>
+    </div>
         </main>
     );
 }
