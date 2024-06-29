0a6796c9df5f701483c74b9e1030804ea26fc728
Azurieum <mlgopnope@gmail.com>
2024-06-28 21:14:13 -0400

    letting data update!

--- app/components/accessPage.tsx
+++ app/components/accessPage.tsx
@@ -26,23 +26,26 @@ const AccessContent = () => {
     try {
       const response = await fetch(`http://${ipAddress}:80/get-data/${password}${username}${hash}`);
       const result = await response.json();
-
-      if (result.status == 200) {
-        setData(result.data);
-        console.log("Data updated successfully:", result.data);
+      if (response.ok) {
+        setData(result); // Update the state with the fetched data
+        console.log("Data updated successfully:", result);
       } else {
-        console.error("API call failed or returned:", result.status);
+        console.error("API call failed or returned:", response.status);
       }
     } catch (error) {
       console.error("Error connecting to the API:", error);
     }
   };
 
+  useEffect(() => {
+    const interval = setInterval(updateData, 400); 
+    return () => clearInterval(interval); 
+  }, []);
+
   const sendCommand = async (command: string) => {
     try {
       const response = await fetch(`http://${ipAddress}:80/send-command/${command}/${password}${username}${hash}`);
       const result = await response.json();
-
       if (response.ok && result.status === "Success") {
         setCommandStatus("COMMAND SUCCESS");
       } else {
@@ -67,10 +70,6 @@ const AccessContent = () => {
     }
   }
 
-  useEffect(() => {
-    const intervalId = setInterval(updateData, 600);
-    return () => clearInterval(intervalId);
-  }, []);
 
   const loopToggle = () => {
     setLoopOn(prevLoopOn => !prevLoopOn);
@@ -87,29 +86,29 @@ const AccessContent = () => {
         <div className="w-full p-4 mb-4 bg-gray-100 dark:bg-gray-800 rounded-lg shadow-md">
           <h2 className="text-lg font-bold dark:text-white mb-2">System Statistics</h2>
           <div className="grid grid-cols-2 gap-4">
-            <span className="text-sm font-semibold dark:text-gray-300">{data.cpu}</span>
-            <span className="text-sm font-semibold dark:text-gray-300">{data.gpu}</span>
+          <span className="text-sm font-semibold dark:text-gray-300">{data && data.cpu ? data.cpu : 'Loading...'}</span>
+          <span className="text-sm font-semibold dark:text-gray-300">{data && data.gpu ? data.gpu : 'Loading...'}</span>
           </div>
           <div className="grid grid-cols-2 gap-4">
             <div className="flex flex-col items-start">
               <span className="text-sm font-semibold dark:text-gray-300">CPU Usage:</span>
-              <span className="text-sm dark:text-white">{data.cpu_usage}</span>
+              <span className="text-sm dark:text-white"><span className="text-sm font-semibold dark:text-gray-300">{data && data.cpu_usage ? data.cpu_usage : 'Loading...'}%</span></span>
             </div>
             <div className="flex flex-col items-start">
               <span className="text-sm font-semibold dark:text-gray-300">Memory Usage:</span>
-              <span className="text-sm dark:text-white">{data.memory_usage}</span>
+              <span className="text-sm font-semibold dark:text-gray-300">{data && data.memory_usage ? data.memory_usage : 'Loading...'}</span>
             </div>
             <div className="flex flex-col items-start">
               <span className="text-sm font-semibold dark:text-gray-300">Runtime:</span>
-              <span className="text-sm dark:text-white">{data.runtime}</span>
+              <span className="text-sm font-semibold dark:text-gray-300">{data && data.runtime ? data.runtime : 'Loading...'}</span>
             </div>
             <div className="flex flex-col items-start">
               <span className="text-sm font-semibold dark:text-gray-300">Wifi Name:</span>
-              <span className="text-sm dark:text-white">{data.wifiname}</span>
+              <span className="text-sm font-semibold dark:text-gray-300">{data && data.wifiname ? data.wifiname : 'Loading...'}</span>
             </div>
             <div className="flex flex-col items-start">
               <span className="text-sm font-semibold dark:text-gray-300">Recall On:</span>
-              <span className="text-sm dark:text-white">{data.recallstatus}</span>
+              <span className="text-sm font-semibold dark:text-gray-300">{data && data.recallstatus ? data.recallstatus : 'Loading...'}</span>
             </div>
             <div className="flex flex-col items-start">
               <span className="text-sm font-semibold dark:text-gray-300">Loop On:</span>
